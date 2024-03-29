29f11b872ae96fd0bc65d8ed1e3a5da1921f16b7
mythreye1998 <35624726+mythreye1998@users.noreply.github.com>
2024-03-29 16:40:28 -0400

    Create customSortString.java

---
+++ customSortString.java
@@ -0,0 +1,24 @@
+/**
+Time Complexity: O(n + m), Space Complexity: O(n). Utilizes HashMap to count character frequencies and iterates through strings to construct the result based on the specified order.
+ */
+import java.util.HashMap;
+
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder result = new StringBuilder();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        for (char c : order.toCharArray()) {
+            if (mp.containsKey(c)) {
+                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+                mp.remove(c);
+            }
+        }
+        for (char c : mp.keySet()) {
+            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+        }
+        return result.toString();
+    }
+}
